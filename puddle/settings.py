dd2bd2bc96d7dd92887ced969d686c2f4a5399bc
Karim Tayie <karimsherif2597@gmail.com>
2023-08-28 01:34:31 +0300

    feat: Made changes for vercel deployment

--- puddle/settings.py
+++ puddle/settings.py
@@ -74,18 +74,13 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "puddle.wsgi.application"
+WSGI_APPLICATION = "puddle.wsgi.app"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {}
 
 
 # Password validation
